ae9c938e98ec8a3fc5f1b15161d7d7d716088174
Sana Zaidi <sana.rizvi.5@gmail.com>
2019-12-18 20:09:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,21 +4,8 @@ class Dog
     @breed = breed 
   end
   
-  def name=(name)
-    @name = name
-  end
- 
-  def name
-    @name
-  end
- 
-  def breed=(breed)
-    @breed = breed
-  end
- 
-  def breed
-    @breed
-  end
-end
- 
- end
+  def breed=(Mutt)
+    @breed = Mutt 
+  end 
+  
+  
