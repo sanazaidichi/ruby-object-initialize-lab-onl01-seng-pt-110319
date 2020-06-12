e0a8ca3d761c898fe97f344e33096ef0cd592c57
sanazaidichi <sana.rizvi.5@gmail.com>
2020-06-12 09:36:57 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,10 @@
 class Dog
-  def initialize(name, breed = "Mutt")
+  #initialize
+  #sets the name of the dog in an instance variable @name
+  
+  def initialize(name, breed = "Mutt") #<-- defaults the breed argument to "Mutt" in an instance variable @breed
     @name = name
+    #sets the breed of the dog in an instance variable @breed
     @breed = breed 
   end
  end  
--- lib/person.rb
+++ lib/person.rb
@@ -1,13 +1,16 @@
 class Person
+   #initialize
+   #sets the name of the person in an instance variable @name
+
   def initialize(name)
     @name = name
   end
  
-  def name=(name)
-    @name = name
-  end
+  #def name=(name)
+  # @name = name
+  #end
  
-  def name
-    @name
-  end
+  #def name
+  # @name
+  #end
 end
